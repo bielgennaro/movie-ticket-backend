59d0ae162af6fadbdc12082f2bf5498b4f20faf5
bielgennaro <zirondigabriel@gmail.com>
2023-09-08 01:01:26 -0300

    help(cloud): Alguém me ajuda pf

--- Startup.cs
+++ Startup.cs
@@ -18,6 +18,27 @@ public class Startup
 
     public IConfiguration Configuration { get; }
 
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); });
+        }
+
+
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                "default",
+                "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddDbContext<MovieTicketApiContext>(options =>
@@ -43,25 +64,5 @@ public class Startup
         services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader()));
-    }
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); });
-        }
-
-        app.UseRouting();
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllerRoute(
-                "Default",
-                "{controller=Home}/{action=Index}/{id?}");
-        });
     }
 }
